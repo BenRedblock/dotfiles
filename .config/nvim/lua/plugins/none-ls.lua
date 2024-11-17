88262b28e15a6946953a802a3e4e70142023351a
BenRedblock <figr@gmx.de>
2024-11-17 15:01:58 +0100

    nvim updates

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,8 +6,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagostics.eslint_d,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier
       },
     })
 
